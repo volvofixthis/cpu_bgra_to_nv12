3947195e6c778fb05547b15fcb5a570764aa9ad5
Костя <incmail0@gmail.com>
2015-04-16 03:46:09 +0300

    Small fixes of .sh files

--- make_eye_test.sh
+++ make_eye_test.sh
@@ -1,2 +1,3 @@
-make run
+#!/bin/sh
+./make_test.sh
 ffplay -f rawvideo -video_size 1920x1080 -pixel_format nv12 test.nv12
--- make_test.sh
+++ make_test.sh
@@ -1,1 +1,5 @@
-ffmpeg -i cool-car-wallpapers-7-2014-hd.jpg -f rawvideo -pix_fmt bgr0 -y test.bgr0
+#!/bin/sh
+make >/dev/null 2>&1
+ffmpeg -loglevel panic -i cool-car-wallpapers-7-2014-hd.jpg -f rawvideo -pix_fmt bgr0 -y test.bgr0
+./cpu_convert
+./cpu_convert_sse
